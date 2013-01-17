1a52c684abd2c1c154924a68d32f58d6bd07ddf0
Mathieu Lorber <mathieu.lorber@gmail.com>
2013-01-17 13:22:14 +0100

    Rename for shorter names (simplest solution for travis-ci) & test.rb script update

--- test.rb
+++ test.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
 
-tests = ['count-and-remove-all', 'edit-and-remove', 'history', 'storage']
-
 # TODO check ko due to false dir is obvious
 # TODO for use './test.rb fmk scenario' (scenario optional)
+
+#agilityjs  angularjs-perf  canjs   dart    emberjs   jquery    knockoutjs  yui
+#angularjs  backbone  closure   dojo    gwt   knockback spine
+
+#casperjs tests/test.js vanilla-examples/vanillajs/index.html
+
+#backbone.xmpp      dijon       meteor        sammyjs
+#backbone_marionette    duel        montage       socketstream
+#batman       epitome       o_O       somajs
+#cujo       extjs       olives        stapes
+#dart       javascriptmvc     plastronjs      thorax
+#derby        knockoutjs_classBindingProvider puremvc       troopjs
+#dermis       maria       rappidjs
+
+tests = ['count', 'edit', 'history', 'storage']
 
 fmks = Hash.new
 fmks['angularjs'] = 'architecture-examples/angularjs/index.html'
@@ -18,27 +31,26 @@ fmks['vanillajs'] = 'vanilla-examples/vanillajs/index.html'
 
 def printok()
   print "\033[32m"
-  printf '%-22s' % 'OK'
+  printf '%-10s' % 'OK'
   print "\033[0m| "
 end
 
 def printko()
   print "\033[31m"
-  printf '%-22s' % 'KO'
+  printf '%-10s' % 'KO'
   print "\033[0m| "
 end
 
-printf '%-22s| ' % ' '
+printf '%-15s| ' % ' '
 
 tests.each do |test|
-  printf '%-22s| ' % test
+  printf '%-10s| ' % test
 end
 
 print "\n"
 
-#casperjs tests/$test.js ${fmks[$fmk]} &> /dev/null && printok || printko
 fmks.keys.each do |key|
-  printf '%21s | ' % key
+  printf '%14s | ' % key
   tests.each do |test|
     system("casperjs tests/#{test}.js #{fmks[key]} &> /dev/null") ? printok : printko
   end
