1cf1cd08e6786ad9ee73fa2025275e713261d7d1
Mathieu Lorber <mathieu.lorber@gmail.com>
2013-01-18 14:18:12 +0100

    Script can be used with args, for example : ./test.rb dart edit

--- test.rb
+++ test.rb
@@ -16,18 +16,28 @@
 #derby        knockoutjs_classBindingProvider puremvc       troopjs
 #dermis       maria       rappidjs
 
-tests = ['count', 'edit', 'history', 'storage']
+$tests = ['count', 'edit', 'history', 'storage']
 
-fmks = Hash.new
-fmks['angularjs'] = 'architecture-examples/angularjs/index.html'
-fmks['angularjs-perf'] = 'architecture-examples/angularjs-perf/index.html'
-fmks['backbone'] = 'architecture-examples/backbone/index.html'
-fmks['dart'] = 'architecture-examples/dart/web/index.html'
-fmks['emberjs'] = 'architecture-examples/emberjs/index.html'
-fmks['jquery'] = 'architecture-examples/jquery/index.html'
-fmks['knockoutjs'] = 'architecture-examples/knockoutjs/index.html'
-fmks['spine'] = 'architecture-examples/spine/index.html'
-fmks['vanillajs'] = 'vanilla-examples/vanillajs/index.html'
+$fmks = Hash.new
+$fmks['angularjs'] = 'architecture-examples/angularjs/index.html'
+$fmks['angularjs-perf'] = 'architecture-examples/angularjs-perf/index.html'
+$fmks['backbone'] = 'architecture-examples/backbone/index.html'
+$fmks['dart'] = 'architecture-examples/dart/web/index.html'
+$fmks['emberjs'] = 'architecture-examples/emberjs/index.html'
+$fmks['jquery'] = 'architecture-examples/jquery/index.html'
+$fmks['knockoutjs'] = 'architecture-examples/knockoutjs/index.html'
+$fmks['spine'] = 'architecture-examples/spine/index.html'
+$fmks['vanillajs'] = 'vanilla-examples/vanillajs/index.html'
+
+# TODO be able to use lists of items (aka './test.rb dart jquery count edit')
+ARGV.each do |arg|
+  if $tests.include?(arg)
+    $test = arg
+  end
+  if $fmks.keys.include?(arg)
+    $fmk = arg
+  end
+end
 
 def printok()
   print "\033[32m"
@@ -43,18 +53,41 @@ end
 
 printf '%-15s| ' % ' '
 
-tests.each do |test|
-  printf '%-10s| ' % test
+if $test.nil?
+  $tests.each do |test|
+    printf '%-10s| ' % test
+  end
+else
+  printf '%-10s| ' % $test
 end
 
 print "\n"
 
-setCasper = "if [ -z ${casperjs} ]; then export casperjs=casperjs; fi"
+$setCasper = "if [ -z ${casperjs} ]; then export casperjs=casperjs; fi"
 
-fmks.keys.each do |key|
-  printf '%14s | ' % key
-  tests.each do |test|
-    system("#{setCasper};$casperjs tests/#{test}.js #{fmks[key]} > /dev/null") ? printok : printko
+def casper(fmk, test)
+  # TODO option to enable casperjs output instead of table
+  #print "#{$setCasper};$casperjs tests/#{test}.js #{$fmks[fmk]} > /dev/null"
+  #print "\n"
+  system("#{$setCasper};$casperjs tests/#{test}.js #{$fmks[fmk]} > /dev/null") ? printok : printko
+end
+
+def doTests(fmk)
+  printf '%14s | ' % fmk
+  if $test.nil?
+    $tests.each do |test|
+      casper(fmk, test)
+    end
+  else
+    casper(fmk, $stest)
   end
   print "\n"
+end
+
+if $fmk.nil?
+  $fmks.keys.each do |fmk|
+    doTests(fmk)
+  end
+else
+  doTests($fmk)
 end
